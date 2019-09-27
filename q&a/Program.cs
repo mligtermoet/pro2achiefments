9ff6a2e830f34a79a1c105e548a7170410f0d987
28470 <54799107+28470@users.noreply.github.com>
2019-09-27 11:43:24 +0200

    Opdracht toegevoegd.

---
+++ q&a/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace q_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+
+
+            string a;
+            Console.WriteLine("Wat doe jij graag in je vrije tijd?");
+            a = Console.ReadLine();
+
+            Console.Clear();
+
+            string b;
+            Console.WriteLine("Welke game speel je veel?");
+            b = Console.ReadLine();
+
+            Console.Clear();
+            
+            string c;
+            Console.WriteLine("Wat eet jij graag?");
+            c = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("{0}", a);
+            Console.WriteLine("De game die ik graag speel is: {0}.", b);
+            Console.WriteLine("Wat ik graag eet is: {0}", c);
+
+        }
+    }
+}
