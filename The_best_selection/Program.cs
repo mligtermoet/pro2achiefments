3ec52c184b9e9a3f0a8b11f4c76a14fe031cc7ac
28470 <54799107+28470@users.noreply.github.com>
2019-10-18 11:23:16 +0200

    Achievement toegevoegd

---
+++ The_best_selection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_best_selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ one_list_to_list/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace one_list_to_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = {"GTA V","Pavlov VR","Roblox","Minecraft","Beatsaber","Call of Duty",
+            "Clash of Clans","Firewatch","RDR2","Project Cars 2"};
+
+            Console.Clear();
+
+            Console.WriteLine("10 games die ik speel:");
+            Console.WriteLine("");
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
