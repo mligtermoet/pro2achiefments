2b8da0d48d9c394df655fe734dbf8c9866e77bf9
28470 <54799107+28470@users.noreply.github.com>
2019-10-31 12:09:54 +0100

    Nieuwe achievement

---
+++ To_Display_or_to_insert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace To_Display_or_to_insert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isGameRunning = true;
+            //int aantal = 0;
+
+            while (isGameRunning == true)
+            {
+                AGAIN:
+                Console.Clear();
+                Console.WriteLine("Wil jij de lijst invullen of weergeven?");
+                Console.WriteLine("Typ: Invullen om de lijst in te vullen. Typ: Weergeven om de lijst weer te geven.");
+                string input = Console.ReadLine();
+                if (input == "Invullen" || input == "invullen")
+                {
+                    Console.Clear();
+                    Console.WriteLine("HI");
+                    Console.ReadLine();
+                } else if (input == "Weergeven" || input == "weergeven")
+                {
+
+                } else
+                {
+                    goto AGAIN;
+                }
+            }
+        }
+    }
+}
