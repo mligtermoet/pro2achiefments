20e8e392e953c0d547dae9ddb6ea88df7ef53f27
28470 <54799107+28470@users.noreply.github.com>
2019-11-01 11:30:15 +0100

    Achievement toegevoegd

--- To_Display_or_to_insert/Program.cs
+++ To_Display_or_to_insert/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 namespace To_Display_or_to_insert
 {
     class Program
     {
+        public static ArrayList lijst = new ArrayList();
         static void Main(string[] args)
         {
 
@@ -12,22 +14,42 @@ namespace To_Display_or_to_insert
 
             while (isGameRunning == true)
             {
-                AGAIN:
+                
                 Console.Clear();
                 Console.WriteLine("Wil jij de lijst invullen of weergeven?");
                 Console.WriteLine("Typ: Invullen om de lijst in te vullen. Typ: Weergeven om de lijst weer te geven.");
+                Console.WriteLine("");
+
                 string input = Console.ReadLine();
                 if (input == "Invullen" || input == "invullen")
                 {
                     Console.Clear();
-                    Console.WriteLine("HI");
-                    Console.ReadLine();
+                    Console.WriteLine("Hoeveel namen wil jij invullen?");
+                    int aantal = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    
+                    for (int i = 0; i < aantal; i++)
+                    {
+                        Console.WriteLine("Typ nu steeds 1 naam en druk op Enter");
+                        input = Console.ReadLine();
+                        lijst.Add(input);
+                        Console.Clear();
+                    }
+                    
                 } else if (input == "Weergeven" || input == "weergeven")
                 {
+                    foreach(String s in lijst)
+                    {
+                        Console.WriteLine(s);
+                        
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("Druk op een knop om terug naar het menu te gaan.");
+                    Console.ReadKey();
+                }
 
-                } else
-                {
-                    goto AGAIN;
+                if (input == "exit") {
+                    isGameRunning = false;
                 }
             }
         }
